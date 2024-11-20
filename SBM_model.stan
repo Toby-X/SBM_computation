20ce2df6cd397d7ba735d87b86ae07a9c32741f4
TobyXu <751280216@qq.com>
2024-11-20 09:41:28 -0500

    spectral

--- SBM_model.stan
+++ SBM_model.stan
@@ -1,35 +1,121 @@
+// data {
+//   int<lower=0> N;
+//   int<lower=0> K;
+//   matrix[N, N] A;
+// }
+// 
+// parameters {
+//   vector<lower=0>[K] alpha;
+//   simplex[K] Pi;
+//   real<lower=0> a;
+//   real<lower=0> b;
+//   int<lower=1,upper=K> Z[N];
+//   matrix<lower=0>[K, K] B_lower;
+// }
+// 
+// transformed parameters {
+//   matrix[K, K] B;
+//   B = B_lower + B_lower' - diag_matrix(diagonal(B_lower));
+// }
+// 
+// model {
+//   for (i in 1:N) {
+//     for (j in 1:i){
+//       int Z1 = Z[i];
+//       int Z2 = Z[j];
+//       A[i,j] ~ bernoulli(B[Z1, Z2]);
+//     }
+//   }
+//   Z ~ categorical(Pi);
+//   Pi ~ dirichlet(alpha);
+//   for (i in 1:K){
+//     for (j in 1:i){
+//       B_lower[i,j] ~ beta(a,b);
+//     }
+//   }
+// }
+
+// data {
+//   int<lower=0> N;
+//   int<lower=0> K;
+//   matrix[N, N] A;
+// }
+// 
+// parameters {
+//   vector<lower=0>[K] alpha;
+//   simplex[K] Pi;
+//   real<lower=0> a;
+//   real<lower=0> b;
+//   int<lower=1,upper=K> Z[N];
+//   matrix<lower=0>[K, K] B_lower;
+// }
+// 
+// transformed parameters {
+//   matrix[K, K] B;
+//   B = B_lower + B_lower' - diag_matrix(diagonal(B_lower));
+// }
+// 
+// model {
+//   for (i in 1:N) {
+//     for (j in 1:i){
+//       int Z1 = Z[i];
+//       int Z2 = Z[j];
+//       A[i,j] ~ bernoulli(B[Z1, Z2]);
+//     }
+//   }
+//   Z ~ categorical(Pi);
+//   Pi ~ dirichlet(alpha);
+//   for (i in 1:K){
+//     for (j in 1:i){
+//       B_lower[i,j] ~ beta(a,b);
+//     }
+//   }
+// }
+
 data {
-  int<lower=0> N;
-  int<lower=0> K;
-  matrix[N, N] A;
+  int<lower=0> N; // Number of nodes
+  int<lower=1> K; // Number of clusters
+  array[N, N] int<lower=0, upper=1> A; // Adjacency matrix
 }
-
 parameters {
-  vector<lower=0>[K] alpha;
-  simplex[K] Pi;
-  real<lower=0> a;
-  real<lower=0> b;
-  int<lower=1,upper=K> Z[N];
-  matrix<lower=0>[K, K] B_lower;
+  vector<lower=0>[K] alpha; // Dirichlet parameters for Pi
+  simplex[K] Pi; // Cluster probabilities
+  real<lower=0> a; // Beta parameter
+  real<lower=0> b; // Beta parameter
+  matrix<lower=0>[K, K] B_lower; // Lower triangular part of B
 }
-
 transformed parameters {
   matrix[K, K] B;
+  // Make B symmetric
   B = B_lower + B_lower' - diag_matrix(diagonal(B_lower));
 }
-
 model {
-  for (i in 1:N) {
-    for (j in 1:i){
-      A[i,j] ~ bernoulli(B[Z[i], Z[j]]);
+  // Priors
+  Pi ~ dirichlet(alpha);
+  for (i in 1 : K) {
+    for (j in 1 : i) {
+      B_lower[i, j] ~ beta(a, b);
     }
   }
-  Z ~ categorical(Pi);
-  Pi ~ dirichlet(alpha);
-  for (i in 1:K){
-    for (j in 1:i){
-      B_lower[i,j] ~ beta(a,b);
+  
+  // Marginalize over Z
+  for (i in 1 : N) {
+    for (j in 1 : i) {
+      real prob = 0;
+      for (k1 in 1 : K) {
+        for (k2 in 1 : k1) {
+          prob += Pi[k1] * Pi[k2] * bernoulli_lpmf(A[i, j] | B_lower[k1, k2]);
+        }
+      }
+      target += log(prob); // Add the marginalized likelihood
     }
+  }
+}
+generated quantities {
+  array[N] int<lower=1, upper=K> Z; // Cluster assignments
+  
+  for (n in 1 : N) {
+    Z[n] = categorical_rng(Pi); // Sample Z from the posterior Pi
   }
 }
 
---
+++ Spectral.R
@@ -0,0 +1,215 @@
+library(gtools)
+library(RSpectra)
+library(e1071)
+library(clue)
+
+# Index Vector 2 0-1 matrix
+vec2mat <- function(x){
+  K <- max(x)
+  N <- length(x)
+  Z <- matrix(0, nrow = N, ncol = K)
+  Z[cbind(1:N, x)] <- 1
+  Z
+}
+
+# 0-1 matrix 2 index vector
+mat2vec <- function(Z){
+  apply(Z, 1, which.max)
+}
+
+# SCORE function
+.find_act <- function(s,P){
+  sn = s+sum(P<=s)
+  while (sn %in% P){
+    sn = sn+1
+  }
+  return(sn)
+}
+
+SCORE <- function(U){
+  K = ncol(U)
+  P = NA
+  ## pseudo-pruning
+  if (any(U[,1]<=1e-7)){
+    P = which(U[,1]<=1e-7)
+    U0 = U[-P,]
+    Us = U0/U0[,1]
+  } else {
+    Us = U/U[,1]
+  }
+  
+  IK = diag(rep(1,K))
+  S = c()
+  Y0 = Us
+  Y = Y0
+  for (k in 1:K) {
+    l = apply(Y,1,norm,"2")
+    S = c(S,which.max(l))
+    u = Y[S[k],]/norm(Y[S[k],],"2")
+    u = data.matrix(u)
+    Y = Y%*%(IK-u%*%t(u))
+  }
+  
+  if (!any(is.na(P))){
+    S = sapply(S,.find_act,P)
+  }
+  return(S)
+}
+
+# L2 normalization
+svm_cone <- function(U, beta=0, max.it=50, eps=1e-12, cond_tre=150){
+  # U is the top eigenvectors
+  # K is the dimension of the data point
+  # beta is a "stablize" parameter, delete points with less norm than beta quantile
+  K = ncol(U)
+  row_norm = apply(U, 1, norm, "2")
+  if (beta != 0){
+    qbeta = as.numeric(quantile(row_norm,beta))
+    del_idx = which(row_norm<qbeta)
+    U = U[-del_idx,]
+    row_norm = row_norm[-del_idx]
+  }
+  Us = U/row_norm
+  
+  # This param is chosen by the code of Xueyu Mao et. al.
+  if (K > 10){
+    nu = 2*K/nrow(U)
+  } else {
+    nu = K/nrow(U)
+  }
+  # One Class SVM
+  one_svm = e1071::svm(rep(1, nrow(Us)) ~ Us, kernel="linear", type="one-classification", 
+                       nu = nu, scale = F)
+  w = t(one_svm$coefs) %*% one_svm$SV
+  b = one_svm$rho
+  SVs = one_svm$SV
+  SV_idx = one_svm$index[rowSums(abs(SVs))>0]
+  b_y = Us %*% t(w)
+  
+  # eps0 and eps1 are actually tuning parameters
+  eps0 = 0
+  eps1 = .02
+  flag = T
+  iter = 0
+  
+  while (flag & iter < max.it) {
+    pure_idx = which(b_y<=b*(1+eps0))
+    SV_idx.new = unique(c(SV_idx,pure_idx))
+    SVs.new = Us[SV_idx.new,]
+    
+    ## Use Vertex Hunting instead of K-means
+    IK = diag(rep(1,K))
+    pure_idx = c()
+    Y0 = SVs.new
+    Y = Y0
+    for (k in 1:K) {
+      l = apply(Y,1,norm,"2")
+      pure_idx = c(pure_idx,which.max(l))
+      u = Y[pure_idx[k],]/norm(Y[pure_idx[k],],"2")
+      u = data.matrix(u)
+      Y = Y%*%(IK-u%*%t(u))
+    }
+    pure_idx = SV_idx.new[pure_idx]
+    
+    vp = U[pure_idx,]
+    np = 1/apply(vp,1,norm,"2")
+    yp = np*vp
+    
+    if (kappa(yp)>cond_tre){
+      flag = T
+      eps0 = eps0 + eps1
+    } else {
+      flag = F
+    }
+    iter <- iter + 1
+  }
+  
+  return(pure_idx)
+}
+
+# Spectral Estimation Algorithms
+spectral_clustering <- function(A, K){
+  svd_A <- RSpectra::svds(A, K)
+  kmeans_res <- kmeans(svd_A$u, K, iter.max = 100, 
+                       nstart = 10, algorithm = "Lloyd")
+  clust_vec <- kmeans_res$cluster
+  Z <- vec2mat(clust_vec)
+  # calculate the B matrix
+  B_half <- solve(t(Z) %*% Z, t(Z) %*% svd_A$u)
+  B <- t(B_half) %*% (svd_A$d * B_half)
+  list(Z = Z, B = B)
+}
+
+spectral_simplex_null <- function(A, K){
+  N <- nrow(A)
+  
+  svd_A <- RSpectra::svds(A, K)
+  if (all(svd_A$u[,1]<=1e-14)){
+    svd_A$u = -svd_A$u
+    svd_A$v = -svd_A$v
+  }
+  U <- svd_A$u
+  IK <- diag(rep(1,K))
+  S <- c()
+  for (k in 1:K) {
+    l <- apply(U,1,norm,"2")
+    S <- c(S,which.max(l))
+    u <- U[S[k],]/norm(U[S[k],],"2")
+    u <- data.matrix(u)
+    U <- U%*%(IK-u%*%t(u))
+  }
+  Z_mixed <- svd_A$u%*%solve(svd_A$u[S,])
+  kmeans_res <- kmeans(Z_mixed, K, iter.max = 100,
+                       nstart = 10, algorithm = "Lloyd")
+  clust_vec <- kmeans_res$cluster
+  Z <- vec2mat(clust_vec)
+  B_half <- solve(t(Z) %*% Z, t(Z) %*% svd_A$u)
+  B <- t(B_half) %*% (svd_A$d * B_half)
+  
+  list(Z = Z, B = B)
+}
+
+spectral_simplex_score <- function(A, K){
+  svd_A <- RSpectra::svds(A, K)
+  if (all(svd_A$u[,1]<=1e-14)){
+    svd_A$u = -svd_A$u
+    svd_A$v = -svd_A$v
+  }
+  S <- SCORE(svd_A$u)
+  
+  Z_mixed <- svd_A$u%*%solve(svd_A$u[S,])
+  kmeans_res <- kmeans(Z_mixed, K, iter.max = 100,
+                       nstart = 10, algorithm = "Lloyd")
+  clust_vec <- kmeans_res$cluster
+  Z <- vec2mat(clust_vec)
+  B_half <- solve(t(Z) %*% Z, t(Z) %*% svd_A$u)
+  B <- t(B_half) %*% (svd_A$d * B_half)
+  
+  list(Z = Z, B = B)
+}
+
+spectral_simplex_l2 <- function(A, K){
+  svd_A <- RSpectra::svds(A, K)
+  if (all(svd_A$u[,1]<=1e-14)){
+    svd_A$u = -svd_A$u
+    svd_A$v = -svd_A$v
+  }
+  S <- svm_cone(svd_A$u)
+  
+  Z_mixed <- svd_A$u%*%solve(svd_A$u[S,])
+  kmeans_res <- kmeans(Z_mixed, K, iter.max = 100,
+                       nstart = 10, algorithm = "Lloyd")
+  clust_vec <- kmeans_res$cluster
+  Z <- vec2mat(clust_vec)
+  B_half <- solve(t(Z) %*% Z, t(Z) %*% svd_A$u)
+  B <- t(B_half) %*% (svd_A$d * B_half)
+  
+  list(Z = Z, B = B)
+}
+
+# Hungarian Algorithm to find the corresponding index
+find_best_idx <- function(EX,X){
+  loss.mad <- t(apply(X, 2, function(x) colMeans(abs(x - EX))))
+  od <- clue::solve_LSAP(loss.mad)
+  od
+}
