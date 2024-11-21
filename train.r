d644a92836d70efd3e701d0d9ad96ba1024f93fd
keanson <keanson@126.com>
2024-11-20 20:14:43 -0500

    Add methods parameter to train function for model comparison

--- train.r
+++ train.r
@@ -4,8 +4,10 @@
 # K: community number
 # beta: power parameter for imbalance (heterogeneity) of the community sizes
 # M: parameter of the connection probability
+# methods: methods to be compared
+
 # return: selected metrics, now is Accuracy, Macro-F1, Micro-F1, NMI, ARI
-train <- function(N, b, K, beta, M){
+train <- function(N, b, K, beta, M, methods=c("Spectral1", "Spectral2", "Spectral3", "Spectral4", "Gibbs", "VB", "VEM", "HMC")) {
     # Data generation
 
     # training model
