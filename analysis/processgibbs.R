2265521f87980c54a9afd0744b700b98c285d499
keanson <keanson@126.com>
2024-11-30 15:16:16 -0500

    Update visualization function to plot B_MSE and adjust x-axis limits; enhance data processing for Gibbs sampling results

--- analysis/processgibbs.R
+++ analysis/processgibbs.R
@@ -4,3 +4,17 @@ for (j in 1:10){
     res_gibbs <- rbind(res_gibbs, res)
 }
 save(res_gibbs, file = "Research/SBM_computation/data/sbm_gibbs_1000.RData")
+
+res_gibbs <- list()
+for (j in 1:10){
+    load(paste0("./sbm_gibbs_seed_", (j-1) * 10 + 1, ".RData"))
+    res_gibbs <- rbind(res_gibbs, res)
+}
+save(res_gibbs, file = "./sbm_gibbs_2000.RData")
+
+res <- list()
+for (j in c(250, 500, 1000, 2000)){
+    load(paste0("./data/sbm_gibbs_", j, ".RData"))
+    res <- rbind(res, res_gibbs)
+}
+save(res, file = "./data/sbm_gibbs.RData")
